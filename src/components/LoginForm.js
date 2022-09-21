309f0d8f21f18c60a41ed86a6a796e705d7405cb
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2022-09-22 04:33:46 +0900

    input값

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import InputBoxs from "./InputBox";
-import Buttons from "./Buttons";
+
+
 
 const LoginBox = styled.div`
   width:230px;
@@ -13,19 +13,47 @@ const LoginBox = styled.div`
   gap:22px;
 `;
 
+//텍스트 박스 스타일 정의
+const InputBox = styled.input`
+  width:228px;
+  height:44px;
+  background:white;
+  border-radius:2px;
+  border:none;
+  outline:none;
+  padding:10px;
+`
+const Button = styled.button`
+  width:228px;
+  height:44px;
+  background:${props => props.color || "#464BF2"};
+  color:white;
+  border:0;
+  border-radius:2px;
+  font-weight:bold;
+`
 
+const LoginHandle= (event) =>{
+  
+  const id = document.getElementById("id").value;
+  const pw = document.getElementById('password').value;
+  alert(`아이디: ${id}, 비밀번호: ${pw}`);
+ 
+  
+  
+}
 
 
 //로그인 폼 작성
 const LoginForm = () => {
   return(
-    <form>
+    <form name="LoginForm" onSubmit={LoginHandle}>
       <LoginBox>
-        <InputBoxs text="아이디를 입력하세요"/>
-        <InputBoxs text="비밀번호를 입력하세요"/>
-        <Buttons text="로그인"/>
-        <Buttons text="아이디 / 비밀번호 찾기"/>
-        <Buttons color="none" text="회원가입"/>
+        <InputBox type="text" id="id" name="id" placeholder="아이디를 입력하세요" autocomplete />
+        <InputBox type="password" id="password" name="password" placeholder="비밀번호를 입력하세요" autocomplete />
+        <Button type="submit">로그인</Button>
+        <Button>아이디 / 비밀번호찾기</Button>
+        <Button color="none">회원가입</Button>
       </LoginBox>
     </form>
   )
