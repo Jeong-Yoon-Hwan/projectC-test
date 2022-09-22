3316023ba4880ba10bbf351b14faee5c1125f5c4
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2022-09-22 11:16:09 +0900

    jsonserver 설치, json-sever에 로그인 데이터 전송

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -38,19 +38,66 @@ const LoginHandle= (event) =>{
   const id = document.getElementById("id").value;
   const pw = document.getElementById('password').value;
   alert(`아이디: ${id}, 비밀번호: ${pw}`);
- 
+
+  //데이터 전송//
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("POST","http://localhost:3001/login");
+  xhr.setRequestHeader("content-type","application/json; charset=UTF-8");
+  
+  const data = {id:`${id}`, pw:`${pw}`};
+  xhr.send(JSON.stringify(data));
+
+  xhr.onload = () =>{
+    if(xhr.status === 201){
+      const res  = JSON.parse(xhr.response);
+      console.log(res);
+    }else{
+      console.log(xhr.status, xhr.statusText);
+    }
+  }
   
   
 }
+
+function getPostData(){
+  const xhr = new XMLHttpRequest();
+  
+  xhr.open("GET","http://localhost:3001/login");
+  xhr.setRequestHeader("content-type","application/json");
+  xhr.send();
+
+  xhr.onload = () =>{
+    if(xhr.status ===200){
+      const res = JSON.parse(xhr.response);
+      console.log(res);
+    }else{
+      console.log(xhr.status, xhr.statusText);
+    }
+  }
+}getPostData();
 
 
 //로그인 폼 작성
 const LoginForm = () => {
   return(
-    <form name="LoginForm" onSubmit={LoginHandle}>
+    <form action="#" name="LoginForm" onSubmit={LoginHandle}>
       <LoginBox>
-        <InputBox type="text" id="id" name="id" placeholder="아이디를 입력하세요" autocomplete />
-        <InputBox type="password" id="password" name="password" placeholder="비밀번호를 입력하세요" autocomplete />
+        <InputBox 
+          type="text" 
+          id="id" 
+          name="id" 
+          placeholder="아이디를 입력하세요" 
+          autocomplete="off"
+        />
+        <InputBox 
+          type="password" 
+          id="password" 
+          name="password" 
+          placeholder="비밀번호를 입력하세요" 
+          autocomplete="off" 
+        />
+        
         <Button type="submit">로그인</Button>
         <Button>아이디 / 비밀번호찾기</Button>
         <Button color="none">회원가입</Button>
