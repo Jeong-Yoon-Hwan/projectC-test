43b3dd33fff4a9a935286ad0081ef07600119c27
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2022-10-03 18:41:04 +0900

    웹소켓 적용해서 채팅창에서 서버로 입력값 전송

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express")
+
 const { WebSocketServer } = require("ws")
 const app = express()
 
 //dist 폴더에 있는 파일을 사용
-app.use(express.static("dist"))
+//app.use(express.static("dist"))
 
 //3000번으로 포트 생성
 app.listen(3000, () => {
@@ -13,7 +14,6 @@ app.listen(3000, () => {
 //포트 3001로 웹소켓 서버 생성
 const wss = new WebSocketServer({ port: 3001 })
 
-
 //웹소켓 연결
 wss.on("connection", (ws, request) => {
   //반복문을 사용하여 모든 클라이언트에게 메세지 전송
@@ -21,4 +21,18 @@ wss.on("connection", (ws, request) => {
     client.send(`새로운 유저가 접속했습니다. 현재유저: ${wss.clients.size}명`)
   })
   console.log(`새로운 유저 접속: ${request.socket.remoteAddress}`)
+
+  ws.on("message",data=>{
+    wss.clients.forEach(client=>{
+      client.send(data.toString());
+    })
+  })
+})
+
+wss.on("connection",(ws,request)=>{
+  ws.on("close",()=>{
+    wss.clients.forEach((client)=>{
+      client.send(`유저 한명이 떠났습니다. 현재 유저: ${wss.clients.size}명`)
+    })
+  })
 })
--- src/components/Chating.js
+++ src/components/Chating.js
@@ -3,9 +3,6 @@ import styled from "styled-components"
 import axios from "axios"
 const ws = new WebSocket("ws://localhost:3001");
 
-function sendMessage(){
-
-}
 
 
 
@@ -21,6 +18,29 @@ const ChatingBox = styled.div`
     flex-direction:column;
     justify-content:end;
     overflow:hidden;
+
+    & > div {
+      display:flex;
+      flex-direction:column;
+      justify-content:center;
+      align-items:end; 
+    }
+
+      & > div > div {
+        width:max-content;
+        height:max-content;
+        max-width:200px;
+        background-color:#F2DA46;
+        font-weight:bold;
+        font-size:14px;
+        display:flex;
+        align-items:center;
+        justify-content:end;
+        word-break:break-all;
+        border-radius:20px 20px 0px 20px;
+        padding:10px;
+        margin:10px;
+      }
   }
   & > section{
     width:inherit;
@@ -51,11 +71,22 @@ const ChatingBox = styled.div`
   }
 `
 
-
-
 const Chating = () => {
 
+  function sendMessage(){
+    const message = document.getElementById("chat");
+    ws.send(message);
+  }
 
+
+  const inputRef = useRef();
+  const clearInput = () =>{
+    inputRef.current.value="";
+  }
+
+  const Box = useRef("");
+
+  
   //서버에서 보낸 메세지 받기
   function receiveMessage(event){
     //채팅이 입력될 박스를 생성
@@ -67,26 +98,15 @@ const Chating = () => {
     //채팅박스-->메인박스 초기화
     const chatBox = document.querySelector("main");
     //채팅박스에 chat 추가함
-    chatBox.appendChild(chat)
+     Box.current.appendChild(chat)
+    //chatBox.appendChild(chat);
   }ws.onmessage = receiveMessage //서버에 데이터가 전송되었을때 함수 실행
 
 
-
-
-
-  const inputRef = useRef();
-  const clearInput = () =>{
-    inputRef.current.value="";
-  }
-
-  const token = localStorage.getItem("token");
-
-  const Box = useRef("");
-
+  //채팅 입력값 useState로 상태 관리
   const [inputValue,setInputValue] = useState({
     chat : ''
   })
-
   const inputChange = (e) =>{
     const {name, value} = e.target
     setInputValue({
@@ -96,34 +116,29 @@ const Chating = () => {
   }
 
 
+  //보내기 버튼 눌렀을때 채팅내용 담은 박스 생성
   const onSubmit = () =>{  
+    //인풋에 입력한 값을 message로 전송
+    const message = inputValue.chat;
+    ws.send(message);
+
     if(inputValue.chat === ""){
       alert("채팅 안치셨어요");
     }else{
-    let newBox = document.createElement('div');
-    newBox.textContent=`${inputValue.chat}`; //new Box에 채팅 인풋에 입력한 값으로 추가
-    newBox.style.backgroundColor="#F2DA46";
-    newBox.style.borderRadius="20px 20px 0px 20px";
-    newBox.style.padding ="10px";
-    newBox.style.margin="10px";
-    newBox.style.width="max-content";
-    newBox.style.maxWidth="200px";
-    newBox.style.height="max-content";
-    newBox.style.fontWeight="bold";
-    newBox.style.fontSize="14px";
-    newBox.style.display="flex";
-    newBox.style.alignItems="center";
-    newBox.style.wordBreak="break-all";
-    Box.current.appendChild(newBox);
-    Box.current.style.display="flex";
-    Box.current.style.flexDirection="column";
-    Box.current.style.justifyContent="end";
-    Box.current.style.alignItems="end";
-    }
-    clearInput();
+    //newBox : 채팅메세지 박스
+    // let newBox = document.createElement('div');
+    // newBox.textContent=`${inputValue.chat}`; //new Box에 채팅 인풋에 입력한 값으로 추가
     
+    // //채팅메세지 박스가 입력되는 박스//
+    // Box.current.appendChild(newBox);
+    // Box.current.style.display="flex";
+    // Box.current.style.flexDirection="column";
+    // Box.current.style.alignItems="end";
+    }
+    
+    clearInput();
   }
-
+//엔터키를 눌렀을때 submit 입력
   const onEnter = (e) =>{
     if(e.key === "Enter"){
       onSubmit();
@@ -145,8 +160,8 @@ const Chating = () => {
           onKeyUp={onEnter}
           ref={inputRef}
         />
-        {/* <button onClick={onSubmit}>보내기</button> */}
-        <button></button>
+        <button onClick={onSubmit}>보내기</button>
+        {/* <button></button> */}
         
       </section>
     </ChatingBox>
@@ -156,6 +171,8 @@ const Chating = () => {
 }
 
 
+
+//페이지가 로드 되었을때 인증값 전송
 window.onload = ()=>{
   axios({
     url: 'http://localhost:5858/auth/inspect',
