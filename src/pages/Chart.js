6c34450a66ad8460ab1f639d1e720d6227fde8cf
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2022-10-10 02:19:42 +0900

    가상화폐 api 데이터로 테이블 출력

--- src/pages/Chart.js
+++ src/pages/Chart.js
@@ -32,36 +32,36 @@ const Chart = () => {
           {
             loading ? <span>Loading...</span> :
             (
-             <table>
-              <thead>
-                <tr>
-                  <th>순위</th>
-                  <th>종목</th>
-                  <th>기호</th>
-                  <th>가격(KRW)</th>
-                  <th>총 시가</th>
-                  <th>거래량(24H)</th>
-                  <th>변동(24H)</th>
-                  <th>변동(7D)</th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                 coins.map((coin,idx)=>(
-                  <tr key={idx}>
-                    <td>{ coin.rank }</td>
-                      <td>{ coin.name }</td>
-                      <td>{ coin.symbol }</td>
-                      <td>{ Number(coin.quotes.KRW.price.toFixed(1)).toLocaleString() }</td>
-                      <td>{ (coin.quotes.KRW.market_cap / 1000000000000).toFixed(2) }T</td>
-                      <td>{ (coin.quotes.KRW.volume_24h / 1000000000000).toFixed(2) }T</td>
-                      <td>{ coin.quotes.KRW.percent_change_24h.toFixed(2) }%</td>
-                      <td>{ coin.quotes.KRW.percent_change_7d.toFixed(2) }%</td>
-                  </tr>
-                 )) 
-                }
-              </tbody>
-             </table>
+            <div className="table-wrapper">
+              <div className="column-title">
+                <div style={{width:"4 %",textAlign:"start"}}>순위</div>
+                <div style={{width:"20%",textAlign:"center"}}>종목</div>
+                <div style={{width:"10%",textAlign:"center"}}>기호</div>
+                <div style={{width:"10%",textAlign:"center"}}>가격(KRW)</div>
+                <div style={{width:"10%",textAlign:"center"}}>총 시가</div>
+                <div style={{width:"10%",textAlign:"center"}}>거래량(24H)</div>
+                <div style={{width:"10%",textAlign:"center"}}>변동(24H)</div>
+                <div style={{width:"10%",textAlign:"center"}}>변동(7D)</div>
+              </div>
+
+              {
+                coins.map((coin,idx)=>(
+                  
+                  <div className="column-item">
+                    <div style={{width:"4%"}}>{coin.rank}</div>
+                    <div style={{width:"20%",textAlign:"center"}}>{coin.name}</div>
+                    <div style={{width:"10%",textAlign:"center"}}>{coin.symbol}</div>
+                    <div style={{width:"10%",textAlign:"center"}}>{(coin.quotes.KRW.price.toFixed(1)).toLocaleString()}</div>
+                    <div style={{width:"10%",textAlign:"center"}}>{(coin.quotes.KRW.market_cap / 1000000000000).toFixed(2)}</div>
+                    <div style={{width:"10%",textAlign:"center"}}>{ (coin.quotes.KRW.volume_24h / 1000000000000).toFixed(2) }</div>
+                    <div style={{width:"10%",textAlign:"center"}}>{ coin.quotes.KRW.percent_change_24h.toFixed(2) }%</div>
+                    <div style={{width:"10%",textAlign:"center"}}>{ coin.quotes.KRW.percent_change_7d.toFixed(2) }%</div>
+                  </div>
+
+                ))
+              }
+            </div>
+
             )
           }
         </section>
@@ -75,11 +75,49 @@ export default Chart;
 const Container = styled.div`
   width:100%;
   height:100%;
-  background-color: beige;
+  background-color: white;
   overflow-y:scroll;
   -ms-overflow-style:none;
-  
+  padding:10px;
+ 
   & > header {
     display: flex;
+    margin:10px;
+  }
+  
+  & > section {
+    
+    .table-wrapper{
+      display: grid;
+      grid-template-columns: 8 1fr;
+      
+    }
+    .column-title{
+      display: flex;
+      justify-content:space-between;
+      gap:20px;
+      padding:1rem;
+      //  border:1px solid black;
+      align-items: center;
+      background-color: #464BF2;
+      color:white;
+      position:sticky;
+      top:-15px;
+      border-radius: 2px;
+    }
+    .column-item{
+      display:flex;
+      justify-content: space-between;
+      padding:1rem;
+      border-radius: 2px;
+     // border:1px solid black;
+      & > div{
+        
+      }
+    }
+
+    .column-item:nth-child(odd){
+      background-color: #e0e0e0;
+    }
   }
 `
